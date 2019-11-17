26001b9385f15c0b4e09d6d1c6cf9725515a5df0
Brot-korb <info@verenafischer.online>
2019-11-17 17:13:07 +0100

    3 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,63 @@ import {Student} from '../model/student'
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ],
-  template: `
-  <div> 
-    <li *ngFor="let student of students"> {{student.firstname}} {{student.lastname}}
 
-    </li>
-  </div>
-  `
 })
 export class AppComponent  {
   
-        
+     /*Eine Liste erstellen, zu der man via Button Werte hinzufügen, 
+     oder löschen kann. Für das Löschen wird ein index eingefügt. */ 
     students = [
       new Student("Alpha", "Müller"),
       new Student("Betta", "Muster"),
       new Student("Gamma", "Hansen")
     ];
+
+    onAddStudent() {
+      this.students.push(
+        new Student("A","B")
+      );
+    }
+
+    onRemoveStudent(index: number) {
+      this.students.splice(index, 1)
+    };
+
+    /* Ein Eingabefeld dessen Eingabe in einem Paragraphen ausgegeben
+    wird */
+
+    inputValue="";
+
+    onInputChange(inputValue: string){
+      this.inputValue = inputValue
+    }
+
+    /* Stoppuhr */
+
+    counterRunning = false;
+    currentTime = 0;
+
+    counterInterval: any;
+
+    onStartCounter() {
+      this.counterRunning=true;
+      this.counterInterval = setInterval(() => {
+        this.currentTime = this.currentTime + 0.1;
+      }, 100);
+
+    }
+    onStopCounter() {
+      this.counterRunning=false;
+      clearInterval(this.counterInterval);
+    }
+    onRemoveCounter(){
+      this.currentTime = 0;
+    }
+
+    /*Components*/
+
+    <app-card></app-card>
+
 
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, CardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
